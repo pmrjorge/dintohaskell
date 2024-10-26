9373a8a92444320cb7fce1777133b00d48773e44
Pedro Jorge <pmrjge@gmail.com>
2024-10-26 20:42:56 +0100

    Performed something before Ordering and comb IO Actions

--- 07/IO.hs
+++ 07/IO.hs
@@ -1,2 +1,8 @@
 module UnderstandingIO where
-    
+import System.IO (openFile, IOMode (ReadMode, WriteMode), hGetContents, hPutStr)
+longCopy :: FilePath -> FilePath -> IO ()
+longCopy src dst = openFile src ReadMode >>= 
+                    hGetContents >>=
+                    \contents -> openFile dst WriteMode >>=
+                    flip hPutStr contents
+
