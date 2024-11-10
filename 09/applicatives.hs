087a3a04c97ae53977370762abef3ac3e5e10572
Pedro Jorge <pmrjge@gmail.com>
2024-11-10 23:19:02 +0000

    Studied applicatives in the space of lists

--- 09/applicatives.hs
+++ 09/applicatives.hs
@@ -1,5 +1,17 @@
-class Functor f => Applicative f where
-    pure :: a -> f a
-    infix 4 <*>
-    (<*>) :: f (a -> b) -> f a -> f b
+module Applicatives where
 
+    data List a = Empty | List a (List a)
+    instance Functor List where
+        fmap _ Empty = Empty
+        fmap f (List a as) = List (f a) (fmap f as)
+
+    instance Applicative List where
+        pure :: a -> List a
+        pure a = List a Empty
+        (<*>) :: List (a -> b) -> List a -> List b
+        Empty <*> _ = Empty
+        List f fs <*> vals = (f <$> vals) `concatList` (fs <*>  vals)
+            where
+                concatList :: List a -> List a -> List a
+                concatList Empty as = as
+                concatList (List a as) bs = List a (concatList as bs)
